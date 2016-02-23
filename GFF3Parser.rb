41242b674d8d3ce2329bb35e3e1ce9dca3555561
tkwsm <takeshik@localhost.localdomain>
2016-02-23 18:05:22 +0900

    edit many for Library and test

--- GFF3Parser.rb
+++ GFF3Parser.rb
@@ -5,102 +5,351 @@ require 'rubygems'
 require 'bio'
 include Bio
 
-class GFF3Parsed
-
-  def initialize
-    @gid = ""
-    @tid = ""
-    @scaffold_id     = ""
-    @direction       = ""
-    @exon_sites      = []
-    @cds_sites       = []
-    @concat_exon_seq = "" 
-    @concat_cds_seq  = "" 
-    @transcript_seq  = "" 
-    @prot_seq        = "" 
-  end
-
-  attr_reader :gid, :tid, :scaffold_id, :direction, :concat_exon_seq, :concat_cds_seq, :transcript_seq, :cds_seq, :prot_seq, :exon_sites, :cds_sites
-
-  def get_gid( gid )
-    @gid = gid
-  end
-
-  def get_tid( tid )
-    @tid = tid
-  end
-
-  def get_scaffold_id( scaffold_id )
-    @scaffold_id = scaffold_id
-  end
-
-  def get_direction( direction )
-    @direction = direction
-  end
-
-  def get_exon_sites( exon_sites )
-    exon_sites.each{|e| @exon_sites << [e[0].to_i, e[1].to_i]}
-    @exon_sites.sort!{|x, y| x[0].to_i <=> y[0].to_i}
-  end
-
-  def get_cds_sites( cds_sites )
-    cds_sites.each{|e| @cds_sites << [e[0].to_i, e[1].to_i]}
-    @cds_sites.sort!{|x, y| x[0].to_i <=> y[0].to_i}
-  end
-
-
-  def parse_nucl_sequence( dna_seq )
-    concat_exon_seq = ""
-    formatted_dna_seq = Sequence::NA.new( dna_seq )
-    @exon_sites.each do |exon|
-      exon_start, exon_end = exon
-      concat_exon_seq += formatted_dna_seq.subseq( exon_start, exon_end )
-    end
-    @concat_exon_seq = concat_exon_seq
-    if    @direction == "+"
-      @transcript_seq = Sequence::NA.new( concat_exon_seq )
-    elsif @direction == "-"
-      @transcript_seq = Sequence::NA.new( concat_exon_seq ).complement
-    else
-      @transcript_seq = Sequence::NA.new( concat_exon_seq )
-      STDERR.puts "please create trancript_seq after direction was initialized"
-    end
-  end
-
-  def parse_cds_sequence( dna_seq )
-    concat_cds_seq = ""
-    formatted_dna_seq = Sequence::NA.new( dna_seq )
-    @cds_sites.each do |cds|
-      cds_start, cds_end = cds
-      concat_cds_seq += formatted_dna_seq.subseq( cds_start, cds_end )
-    end
-    @concat_cds_seq = concat_cds_seq
-    if    @direction == "+"
-      @cds_seq = Sequence::NA.new( concat_cds_seq )
-    elsif @direction == "-"
-      @cds_seq = Sequence::NA.new( concat_cds_seq ).complement
-    else
-      @cds_seq = Sequence::NA.new( concat_cds_seq )
-      STDERR.puts "please create cds_seq after direction was initialized"
-    end
-    @protein_seq = @cds_seq.translate
-  end
-
-  def show_transcript_sequence
-    @transcript_seq.to_fasta( @tid, 60 )
-  end
-
-  def show_protein_sequence
-    @protein_seq.to_fasta( @tid, 60 )
-  end
-
-  def show_cds_sequence
-    @cds_seq.to_fasta( @tid, 60 )
-  end
-
-end
 
 module GFF3Parser
+
+  class GFF3Exon
+
+    def initialize( exon_gff3_array, i )
+      @exonnumber = (i+1)
+      @parenttranscriptid = exon_gff3_array[8]
+      @start      = exon_gff3_array[3].to_i
+      @end        = exon_gff3_array[4].to_i
+      @strand     = exon_gff3_array[6]
+    end
+
+    attr_accessor :exonnumber, :start, :end, :strand, 
+                  :parenttranscriptid
+
+  end
+
+  class GFF3Intron
+
+    def initialize( intron_gff3_array, i )
+      @intronnumber = (i+1)
+      @parenttranscriptid = intron_gff3_array[8]
+      @start      = intron_gff3_array[3].to_i
+      @end        = intron_gff3_array[4].to_i
+      @strand     = intron_gff3_array[6]
+    end
+
+    attr_accessor :intronnumber, :start, :end, :strand, 
+                  :parenttranscriptid
+
+  end
+
+  class GFF3TS
+
+    def initialize( ts_gff3_array )
+      @parenttranscriptid = ts_gff3_array[8]
+      @start      = ts_gff3_array[3].to_i
+      @end        = ts_gff3_array[4].to_i
+      @strand     = ts_gff3_array[6]
+    end
+
+    attr_accessor :start, :end, :strand, :parenttranscriptid
+
+  end
+
+  class GFF3TE
+
+    def initialize( te_gff3_array )
+      @parenttranscriptid = te_gff3_array[8]
+      @start      = te_gff3_array[3].to_i
+      @end        = te_gff3_array[4].to_i
+      @strand     = te_gff3_array[6]
+    end
+
+    attr_accessor :start, :end, :strand, :parenttranscriptid
+
+  end
+
+  class GFF3StartCodon
+
+    def initialize( sc_gff3_array )
+      @parenttranscriptid = sc_gff3_array[8]
+      @start      = sc_gff3_array[3].to_i
+      @end        = sc_gff3_array[4].to_i
+      @strand     = sc_gff3_array[6]
+    end
+
+    attr_accessor :start, :end, :strand, :parenttranscriptid
+
+  end
+
+  class GFF3StopCodon
+
+    def initialize( sc_gff3_array )
+      @parenttranscriptid = sc_gff3_array[8]
+      @start      = sc_gff3_array[3].to_i
+      @end        = sc_gff3_array[4].to_i
+      @strand     = sc_gff3_array[6]
+    end
+
+    attr_accessor :start, :end, :strand, :parenttranscriptid
+
+  end
+
+  class GFF3CDS
+
+    def initialize( cds_gff3_array, i )
+      @cdsnumber = (i+1)
+      @parenttranscriptid = cds_gff3_array[8]
+      @start      = cds_gff3_array[3].to_i
+      @end        = cds_gff3_array[4].to_i
+      @strand     = cds_gff3_array[6]
+    end
+
+    attr_accessor :cdsnumber, :start, :end, :strand, 
+                  :parenttranscriptid
+
+  end
+
+  class GFF3UTR5
+
+    def initialize( utr5_gff3_array, i )
+      @exonnumber = (i+1)
+      @parenttranscriptid = utr5_gff3_array[8]
+      @start      = utr5_gff3_array[3].to_i
+      @end        = utr5_gff3_array[4].to_i
+      @strand     = utr5_gff3_array[6]
+    end
+
+    attr_accessor :utr5number, :start, :end, :strand, 
+                  :parenttranscriptid
+
+  end
+
+  class GFF3UTR3
+
+    def initialize( utr3_gff3_array, i )
+      @exonnumber = (i+1)
+      @parenttranscriptid = utr3_gff3_array[8]
+      @start      = utr3_gff3_array[3].to_i
+      @end        = utr3_gff3_array[4].to_i
+      @strand     = utr3_gff3_array[6]
+    end
+
+    attr_accessor :utr3number, :start, :end, :strand, 
+                  :parenttranscriptid
+
+  end
+
+  class GFF3mRNA
+
+    def initialize( mrna_start, mrna_end ) 
+      @start      = mrna_start
+      @end        = mrna_end
+    end
+
+    attr_accessor :start, :end
+
+  end
+
+  class GFF3Transcript
+
+    def initialize( transcriptid )
+
+      @transcriptid  = transcriptid
+      @parentgeneid  = ""
+      @exons         = []
+      @exonposisions = []
+      @cdss          = []
+      @utr5s         = []
+      @utr3s         = []
+      @introns       = []
+      @mrna          = []
+
+    end
+
+    attr_accessor :transcriptid, :parentgeneid, :mrna, :exons, :cdss, 
+                  :utr5s, :utr3s, :start_codon, :stop_codon, :introns, 
+                  :transcription_start_site, :transcription_end_site
+
+    def sort_exon_sites
+
+      @exons.sort!{|ex, ey| ex.start <=> ey.start }
+
+    end
+
+    def sort_cds_sites
+
+      @cdss.sort!{|cx, cy| cx.start <=> cy.start }
+
+    end
+
+    def parsed_exon_sites
+      @exons.collect{|ex| [ex.start, ex.end]}
+    end
+
+    def parsed_cds_sites
+      @cdss.collect{|cx| [cx.start, cx.end]}
+    end
+
+  end
+
+  class GFF3Gene
+
+    def initialize( geneid )
+
+      @geneid          = geneid
+      @transcripts     = []
+      @transcriptshash = {}
+
+    end
+
+    attr_accessor :geneid, :transcripts
+
+    def atranscript( tid )
+      @transcriptshash[ tid ] 
+    end
+
+    def set_atranscript( tid, transcript )
+      @transcriptshash[ tid ] = transcript
+    end
+
+  end
+
+  class GFF3Parsed
+  
+    include GFF3Parser
+
+    def initialize( in_gff, in_scaffold )
+      @exon_sites      = []
+      @cds_sites       = []
+      @concat_exon_seq = "" 
+      @concat_cds_seq  = "" 
+      @transcript_seq  = "" 
+      @prot_seq        = "" 
+      @gffh            = {}
+      @scaffoldh       = {}
+      @genes           = []
+      @geneshash       = {}
+      @transcriptshash = {}
+      @geneids         = []
+      @transcriptids   = []
+      @type_array = define_type_array
+      main_parser2( in_gff, @gffh )
+      main_parser3( in_scaffold, @scaffoldh )
+      create_genes
+    end
+
+    attr_reader :concat_exon_seq, 
+                :concat_cds_seq, :transcript_seq, :cds_seq, 
+                :prot_seq, :exon_sites, :cds_sites
+
+    attr_accessor :gffh, :scaffoldh, :genes, :geneids, :transcriptids
+
+    def create_genes 
+      @geneids = @gffh.keys.sort
+      @gffh.each_key{ |gid| @genes << GFF3Gene.new( gid ) }
+      @genes.each do |gene|
+        @geneshash[ gene.geneid ] = gene
+        @gffh[ gene.geneid ].keys.each do |tid|
+          next if tid == "gene"
+          transcript = GFF3Transcript.new( tid )
+          gene.set_atranscript( tid, transcript )
+          transcript.parentgeneid = gene.geneid
+          gid = gene.geneid
+          @type_array.each do |ftype|
+            data_array = @gffh[gid][tid][ftype]
+            if    ftype == "mRNA"
+              mrna_start = data_array[0][3].to_i
+              mrna_end   = data_array[0][4].to_i
+              transcript.mrna         = GFF3mRNA.new( mrna_start, mrna_end )
+            elsif ftype == "exon"
+              data_array.each_with_index do |aexon, i|
+                transcript.exons << GFF3Exon.new( aexon, i ) 
+                transcript.cdss << GFF3CDS.new( aexon, i )   ###
+              end
+            elsif ftype == "CDS"
+              data_array.each_with_index do |acds, i|
+                transcript.cdss << GFF3CDS.new( acds, i )
+              end
+            elsif ftype == "5-utr" or ftype == "five_prime_UTR"
+              data_array.each_with_index do |a5utr, i|
+                transcript.utr5s << GFF3UTR5.new( a5utr, i )
+              end
+            elsif ftype == "3-utr" or ftype == "three_prime_UTR"
+              data_array.each_with_index do |a3utr, i|
+                transcript.utr3s << GFF3UTR3.new( a3utr, i )
+              end
+            elsif ftype == "intron"
+              data_array.each_with_index do |intron, i|
+                transcript.introns << GFF3Intron.new( intron, i )
+              end
+            elsif ftype == "start_codon"
+              transcript.start_codon = GFF3StartCodon.new( data_array )
+            elsif ftype == "stop_codon"
+              transcript.stop_codon  = GFF3StopCodon.new( data_array )
+            elsif ftype == "transcription_start_site"
+              transcript.transcription_start_site = GFF3TS.new( data_array )
+            elsif ftype == "transcription_end_site"
+              transcript.transcription_end_site   = GFF3TE.new( data_array )
+            else
+              p [ tid, ftype ]; p "something strange! 1"; exit;
+            end
+          end
+          gene.transcripts << transcript
+        end
+      end
+    end
+
+    def agene( gid )
+      @geneshash[ gid ] 
+    end
+
+    def parse_nucl_sequence( dna_seq )
+      concat_exon_seq = ""
+      formatted_dna_seq = Sequence::NA.new( dna_seq )
+      @exon_sites.each do |exon|
+        exon_start, exon_end = exon
+        concat_exon_seq += formatted_dna_seq.subseq( exon_start, exon_end )
+      end
+      @concat_exon_seq = concat_exon_seq
+      if    @direction == "+"
+        @transcript_seq = Sequence::NA.new( concat_exon_seq )
+      elsif @direction == "-"
+        @transcript_seq = Sequence::NA.new( concat_exon_seq ).complement
+      else
+        @transcript_seq = Sequence::NA.new( concat_exon_seq )
+        STDERR.puts "Create trancript_seq after direction was initialized"
+      end
+    end
+
+    def parse_cds_sequence( dna_seq )
+      concat_cds_seq = ""
+      formatted_dna_seq = Sequence::NA.new( dna_seq )
+      @cds_sites.each do |cds|
+        cds_start, cds_end = cds
+        concat_cds_seq += formatted_dna_seq.subseq( cds_start, cds_end )
+      end
+      @concat_cds_seq = concat_cds_seq
+      if    @direction == "+"
+        @cds_seq = Sequence::NA.new( concat_cds_seq )
+      elsif @direction == "-"
+        @cds_seq = Sequence::NA.new( concat_cds_seq ).complement
+      else
+        @cds_seq = Sequence::NA.new( concat_cds_seq )
+        STDERR.puts "please create cds_seq after direction was initialized"
+      end
+      @protein_seq = @cds_seq.translate
+    end
+
+    def show_transcript_sequence
+      @transcript_seq.to_fasta( @tid, 60 )
+    end
+
+    def show_protein_sequence
+      @protein_seq.to_fasta( @tid, 60 )
+    end
+
+    def show_cds_sequence
+      @cds_seq.to_fasta( @tid, 60 )
+    end
+
+  end # Class GFF3Parsed
+
 
   def sampletest
     "good result"
@@ -123,11 +372,15 @@ module GFF3Parser
 
   end # create_gene_hash
 
+  def define_type_array 
+    type_array = %w( CDS exon intron start_codon stop_codon mRNA transcription_end_site transcription_start_site 3-utr 5-utr)
+    return type_array 
+  end
 
   def create_transcript_hash
   
     h = {}
-    type_array = %w( CDS exon intron start_codon stop_codon mRNA transcription_end_site transcription_start_site 3-utr 5-utr)
+    type_array = define_type_array
     type_array.each do |type|
       h[type] = []
     end
@@ -136,36 +389,20 @@ module GFF3Parser
 
   end # create_transcript_hash
 
-#  def sort_features( features_array )
-#  
-#    if features_array == []
-#    elsif features_array[0][6] == "+"
-#      features_array.sort!{|x, y| x[3].to_i <=> y[3].to_i }
-#    elsif features_array[0][6] == "-"
-#      features_array.sort!{|x, y| y[3].to_i <=> x[3].to_i }
-#    else
-#      p "something wrong #{features_array[0]} 2"
-#      p "something wrong #{features_array[0][6]} 3"
-#      exit
-#    end
-#    return features_array
-#
-#  end # sort_features( features_array )
-
   def parse_gff3( line, ftype, h )
 
-    dataline = []
-    seqid  = "" ;  
-    source = ""; 
-    sstart = 0; 
-    send   = 0; 
-    score = ".";
-    strand = "+"; 
-    phase  = "."; 
+    dataline   = []
+    seqid      = "" ;  
+    source     = ""; 
+    sstart     = 0; 
+    send       = 0; 
+    score      = ".";
+    strand     = "+"; 
+    phase      = "."; 
     attributes = "";
-    gid    = ""; 
-    tid = "";
-    type_array = %w( CDS exon intron start_codon stop_codon transcription_end_site transcription_start_site 5-utr 3-utr )
+    gid        = ""; 
+    tid        = "";
+    type_array = define_type_array
   
     a = line.chomp.split("\s")
     seqid  = a[0]
@@ -278,9 +515,8 @@ module GFF3Parser
   
   end # parse_gff3( line, ftype, h )
 
-  def create_scaffold_hash( input_scaffold )
+  def create_scaffold_hash( input_scaffold, scaffold_h )
     
-    scaffold_h = {}
     ff = FlatFile.new(FastaFormat, open(input_scaffold) )
     ff.each do |e|
       scaffold_id = e.definition.slice(/(\S+)/, 1)
@@ -290,7 +526,7 @@ module GFF3Parser
 
   end
 
-  def create_gff_hash( input_gff )
+  def create_gff_hash( input_gff, h )
 
     scaffold_id = ""
     transcript_id = ""
@@ -298,7 +534,6 @@ module GFF3Parser
     prot_seq = ""
     cdna = ""
     a = []
-    h = {}
     ftype = ""
     open( input_gff ).each_with_index do |x, i|
       a = x.chomp.split("\t")
@@ -337,119 +572,70 @@ module GFF3Parser
         exit
       end
     end  # open( input_gff )
-    return h
   end
 
-  def parse_exon_sites( exon_values )
-    exon_sites = []
-    exon_values.each do |e|
-      exon_sites << e[3..4]
-    end
-    exon_sites.sort!{|ex, ey| ex[0].to_i <=> ey[0].to_i}
-    return exon_sites
-  end
-
-  def parse_cds_sites( cds_values )
-    cds_sites = []
-    cds_values.each do |e|
-      cds_sites << e[3..4]
-    end
-    cds_sites.sort!{|ex, ey| ex[0].to_i <=> ey[0].to_i}
-    return cds_sites
-  end
-
-  def main_parser2( in_gff )
-
-    gffh = create_gff_hash( in_gff )
-    return gffh
-
-  end
-
-#  def create_scagffh( gffh )
-#
-#    scagffh = {}
-#    gffh.each_key do |gid|
-#      scaid = gffh[gid]["gene"][0]
-#      scagffh[ scaid ] = [] if scagffh[ scaid ] == nil
-#      scagffh[ scaid ] << gid
+#  def parse_exon_sites( exon_values )
+#    exon_sites = []
+#    exon_values.each do |e|
+#      exon_sites << e[3..4]
 #    end
-#    return scagffh
+#    exon_sites.sort!{|ex, ey| ex[0].to_i <=> ey[0].to_i}
+#    return exon_sites
+#  end
 #
+#  def parse_cds_sites( cds_values )
+#
+#    if    cds_values[0].strand == "+"
+#      cds_values.sort!{|ex, ey| ex.start <=> ey.start }
+#    elsif cds_values[0].strand == "-"
+#      cds_values.sort!{|ex, ey| ey.end   <=> ex.end   }
+#    end
+#    return cds_values
 #  end
 
-  def main_parser3( in_scaffold )
+  def main_parser2( in_gff, h)
 
-    scah = create_scaffold_hash( in_scaffold )
+    h = create_gff_hash( in_gff, h )
+    return h
+
+  end
+
+  def main_parser3( in_scaffold, scah )
+
+    scah = create_scaffold_hash( in_scaffold, scah )
     return scah
 
   end
 
-  def main_parser4( in_gff, in_scaffold )
-
-    gffh = create_gff_hash( in_gff )
-    scah = create_scaffold_hash( in_scaffold )
-
-    gffh.each_key do |gid|
-      gffh[gid].each_key do |tid|
-        next if tid == "gene"
-        cdnas = GFF3Parsed.new
-        cdnas.get_tid( tid )
-
-        exon_values = gffh[gid][tid]["exon"]
-        cds_values = gffh[gid][tid]["CDS"]
-
-        scaffold_id = gffh[gid][tid]["mRNA"][0][0]
-        direction   = gffh[gid][tid]["mRNA"][0][6]
-
-        exons = parse_exon_sites( exon_values ) 
-        cdss  = parse_cds_sites( cds_values ) 
-
-        cdnas.get_exon_sites( exons )
-        cdnas.get_cds_sites( cdss )
-        cdnas.get_scaffold_id( scaffold_id )
-        cdnas.get_direction( direction )
-        dna_seq = scah[ scaffold_id ]
-        cdnas.parse_nucl_sequence( dna_seq )
-        cdnas.parse_cds_sequence( dna_seq )
-      end
-    end
-
-  end  #  main_parser2
-
-  def main_parser( in_gff, in_scaffold, out_cdna, out_prot, out_cdsnucl )
-
-    gffh = GFF3Parser.create_gff_hash( in_gff )
-    scah = GFF3Parser.create_scaffold_hash( in_scaffold )
-
-    gffh.each_key do |gid|
-      gffh[gid].each_key do |tid|
-        next if tid == "gene"
-        cdnas = GFF3Parsed.new
-        cdnas.get_tid( tid )
-
-        exon_values = gffh[gid][tid]["exon"]
-        cds_values = gffh[gid][tid]["CDS"]
-
-        scaffold_id = gffh[gid][tid]["mRNA"][0][0]
-        direction   = gffh[gid][tid]["mRNA"][0][6]
-
-        exons = GFF3Parser.parse_exon_sites( exon_values ) 
-        cdss  = GFF3Parser.parse_cds_sites( cds_values ) 
-
-        cdnas.get_exon_sites( exons )
-        cdnas.get_cds_sites( cdss )
-        cdnas.get_scaffold_id( scaffold_id )
-        cdnas.get_direction( direction )
-        dna_seq = scah[ scaffold_id ]
-        cdnas.parse_nucl_sequence( dna_seq )
-        cdnas.parse_cds_sequence( dna_seq )
-        out_cdna.print cdnas.show_transcript_sequence
-        out_prot.print cdnas.show_protein_sequence
-        out_cdsnucl.print cdnas.show_cds_sequence
-      end
-    end
-
-  end  #  main_parser
+#  def main_parser( in_gff, in_scaffold, out_cdna, out_prot, out_cdsnucl )
+#
+#    scah = GFF3Parser.create_scaffold_hash( in_scaffold )
+#
+#    h.each_key do |gid|
+#      gffh[gid].each_key do |tid|
+#        next if tid == "gene"
+#        cdnas = GFF3Parsed.new
+#        cdnas.get_tid( tid )
+#
+#        exon_values = gffh[gid][tid]["exon"]
+#        cds_values = gffh[gid][tid]["CDS"]
+#
+#        scaffold_id = gffh[gid][tid]["mRNA"][0][0]
+#        direction   = gffh[gid][tid]["mRNA"][0][6]
+#
+#        exons = GFF3Parser.parse_exon_sites( exon_values ) 
+#        cdss  = GFF3Parser.parse_cds_sites( cds_values ) 
+#
+#        dna_seq = scah[ scaffold_id ]
+#        cdnas.parse_nucl_sequence( dna_seq )
+#        cdnas.parse_cds_sequence( dna_seq )
+#        out_cdna.print cdnas.show_transcript_sequence
+#        out_prot.print cdnas.show_protein_sequence
+#        out_cdsnucl.print cdnas.show_cds_sequence
+#      end
+#    end
+#
+#  end  #  main_parser
 
 end # module GFF3Parser
 
--- test_GFF3Parser.rb
+++ test_GFF3Parser.rb
@@ -11,10 +11,7 @@ class TC_GFF3Parser < MiniTest::Unit::TestCase
   end
 
   def setup
-#    @speAgid        = "SPU_025302"
-#    @path2speApepfa = "sample_data/spur_protein.fas"
     @path2speAscafa = "sample_data/spur_scaffold.fas"
-#    @path2speBscafa = "sample_data/hpul_scaffold.fas"
     @path2speAgff = "./sample_data/spur_sample.gff"
     @testgenehash = { "gene" => [] }
     @testtranscripthash = { "CDS"                      => [],
@@ -27,7 +24,7 @@ class TC_GFF3Parser < MiniTest::Unit::TestCase
                             "transcription_start_site" => [],
                             "3-utr"                    => [],
                             "5-utr"                    => [] }
-    @g3p = Test_GFF3Parser.new
+    @g3p = Test_GFF3Parser::GFF3Parsed.new( @path2speAgff, @path2speAscafa )
   end
 
   def test_sampletest
@@ -48,9 +45,9 @@ class TC_GFF3Parser < MiniTest::Unit::TestCase
   end
 
   def test_parse_gff3
-    h     = {}
     ftype = "gene"
     line  = "Scaffold881     GLEAN3-UTR      gene    59053   94582   .       -       .       ID=SPU_025302gn;Name=SPU_025302;"
+    h = {}
     assert_equal( ["SPU_025302gn"], @g3p.parse_gff3( line, ftype, h ).keys )
     assert_equal( "Scaffold881",    @g3p.parse_gff3( line, ftype, h )["SPU_025302gn"]["gene"][0] )
     assert_equal( "59053", @g3p.parse_gff3( line, ftype, h )["SPU_025302gn"]["gene"][3] )
@@ -59,35 +56,46 @@ class TC_GFF3Parser < MiniTest::Unit::TestCase
   end
 
   def test_create_scaffold_hash
-    assert_equal( 397093, @g3p.create_scaffold_hash( @path2speAscafa )["Scaffold881"].size )
+    assert_equal( 397093, @g3p.scaffoldh["Scaffold881"].size )
   end
 
   def test_create_gff_hash
-    assert_equal( ["gene", "SPU_025302-tr"], @g3p.create_gff_hash( @path2speAgff )["SPU_025302gn"].keys )
+    assert_equal( ["gene", "SPU_025302-tr"], @g3p.gffh["SPU_025302gn"].keys )
   end
 
   def test_parse_exon_sites
-    exon_values = @g3p.create_gff_hash( @path2speAgff )["SPU_025302gn"]["SPU_025302-tr"]["exon"] 
-    assert_equal( ["61717", "62175"], @g3p.parse_exon_sites( exon_values )[0] )
+    @g3p.agene( "SPU_025302gn" ).atranscript( "SPU_025302-tr" ).sort_exon_sites
+    exon_values = @g3p.agene( "SPU_025302gn" ).atranscript( "SPU_025302-tr" ).parsed_exon_sites
+    assert_equal( [[61717, 62175], [63168, 63380], [64349, 64480], [88680, 88763], [89086, 89453], [94546, 94582]], exon_values  )
   end
 
   def test_parse_cds_sites
-    cds_values  = @g3p.create_gff_hash( @path2speAgff )["SPU_025302gn"]["SPU_025302-tr"]["exon"]
-    assert_equal( ["61717", "62175"], @g3p.parse_cds_sites( cds_values )[0] )
+    @g3p.agene( "SPU_025302gn" ).atranscript( "SPU_025302-tr" ).sort_cds_sites
+    cds_values  = @g3p.agene( "SPU_025302gn" ).atranscript( "SPU_025302-tr" ).parsed_cds_sites
+    assert_equal( [61717, 62175],  cds_values[0] )
   end
 
   def test_main_parser2
-    assert_equal( "Scaffold881", @g3p.main_parser2( @path2speAgff )["SPU_025302gn"]["SPU_025302-tr"]["mRNA"][0][0] )
+    assert_equal( "Scaffold881", @g3p.gffh["SPU_025302gn"]["SPU_025302-tr"]["mRNA"][0][0] )
+#    assert_equal( "Scaffold881", @g3p.agene( "SPU_025302gn" ).atranscript( "SPU_025302-tr" )["mRNA"][0][0] )
+    assert_equal( 59053, @g3p.agene( "SPU_025302gn" ).atranscript("SPU_025302-tr").mrna.start )
+    assert_equal( 94582, @g3p.agene( "SPU_025302gn" ).atranscript("SPU_025302-tr").mrna.end )
   end
 
   def test_main_parser3
-    assert_equal( 397093, @g3p.main_parser3( @path2speAscafa )["Scaffold881"].size )
+    assert_equal( 397093, @g3p.scaffoldh["Scaffold881"].size )
   end
 
   def test_main_parser4
-    assert_equal( "exon", @g3p.main_parser4( @path2speAgff, @path2speAscafa )["SPU_003298gn"]["SPU_003298-tr"]["exon"][1][2] )
+#    assert_equal( "exon", @g3p.gffh["SPU_003298gn"]["SPU_003298-tr"]["exon"][1][2] )
+    assert_equal( 324991, @g3p.agene( "SPU_003298gn" ).atranscript( "SPU_003298-tr" ).exons[0].start )
+    assert_equal( 94546, @g3p.agene( "SPU_025302gn" ).atranscript( "SPU_025302-tr" ).exons[0].start )
+    gidarray = ["SPU_003297gn", "SPU_003298gn", "SPU_011238gn", "SPU_013499gn", "SPU_015325gn", "SPU_017568gn", "SPU_018425gn", "SPU_022816gn", "SPU_022817gn", "SPU_025302gn", "SPU_025908gn", "SPU_025909gn", "SPU_025910gn"]
+    assert_equal( gidarray, @g3p.geneids )
   end
 
+=begin
+=end
 
 end
 
